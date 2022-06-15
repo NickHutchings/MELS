2fcf3654a30abcd6295c37e28e7b45fbf71af3ff
NickHutchings <33897682+NickHutchings@users.noreply.github.com>
2022-06-15 09:07:20 +0200

    first test

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Xml.Linq;
+using System.Diagnostics;
+namespace FarmGHGcalc
+{
+    class Program
+    {
+        //args[0] and args[1] are farm number and scenario number respectively
+        static void Main(string[] args)
+        {
+            model mod = new model();
+            mod.run(args);
+        }
+    }
+}
+// first test
